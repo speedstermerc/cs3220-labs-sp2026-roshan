2bc8870c29c13de74dc546aee23a319b1b0ac08c
speedstermerc <roshansaigal25@gmail.com>
2026-01-26 15:41:41 -0500

    finished quesition 5 no testing.

--- lab0/5-fsm/Q5.v
+++ lab0/5-fsm/Q5.v
@@ -7,6 +7,32 @@ module fsm (
     input seq,
     output detected );// seq detector 1101, once detected, output 1 for 1 clock cycle
 
-    assign detected = 0;
+    reg [2:0] state, next_state;
+
+    localparam START = 3'd0,
+        A     = 3'd1,
+        B     = 3'd2,
+        C     = 3'd3,
+        D     = 3'd4;
+
+    always @(posedge clk) begin
+        if (reset) begin
+            state <= START;
+        end else begin
+            state <= next_state;
+        end
+    end
+
+    always @(*) begin
+        case (state)
+            START: next_state = seq ? A : START;
+            A: next_state = seq ? B : START;
+            B: next_state = seq ? B : C;
+            C: next_state = seq ? D : START;
+            D: next_state = seq ? B : START;
+        endcase
+    end
+
+    assign detected = (state == D);
     
 endmodule
