8a001fd3078042e3a435f894700e901cd5b3985f
speedstermerc <roshansaigal25@gmail.com>
2026-01-26 12:25:54 -0500

    Q3 finished not tested

--- lab0/2-vector_signals/2-vector_signals.md
+++ lab0/2-vector_signals/2-vector_signals.md
@@ -12,7 +12,7 @@ type specifies the datatype of the vector. This is usually wire or reg. If y
 
 ```
 wire [7:0] w;         // 8-bit wire
-reg  [4:1] x;         // 4-bit reg
+reg  [4:1] x;         // 4-bit reg... the [4:1] just create labels.
 output reg [0:0] y;   // 1-bit reg that is also an output port (this is still a vector)
 input wire [3:-2] z;  // 6-bit wire input (negative ranges are allowed)
 output [3:0] a;       // 4-bit output wire. Type is 'wire' unless specified otherwise.
--- lab0/3-module_hierarchy/Q3.v
+++ lab0/3-module_hierarchy/Q3.v
@@ -6,15 +6,50 @@ module module_hierarchy (
     input [31:0] a,
     input [31:0] b,
     output [31:0] sum
-);//
+);
 
-    assign sum = 32'd0;
+    wire carry_middle;
+    
+    add16 low_adder (
+        .a(a[15:0]),
+        .b(b[15:0]),
+        .cin(1'b0),          // The very first carry-in is 0
+        .sum(sum[15:0]),
+        .cout(carry_middle)   // This "output" goes into the middle wire
+    );
+
+    wire dont_care;
+
+    add16 high_adder (
+        .a(a[31:16]),
+        .b(b[31:16]),
+        .cin(carry_middle),
+        .sum(sum[31:16]),
+        .cout(dont_care)
+    );
+
+
+    // assign sum = 32'd0;
 
 endmodule
 
+//module add16 ( input[15:0] a, input[15:0] b, input cin, output[15:0] sum, output cout );
+
 module add1 ( input a, input b, input cin,   output sum, output cout );
 
-    assign sum = 1'b0;
-    assign cout = 1'b0;
+    // assign sum = (~cin & ~a & b) | (~cin & a & ~b) | (cin & ~a & ~b) | (cin & a & b);
+    // use the xor trick for cleaner code.
+    assign sum = a ^ b ^ cin;
+    assign cout = (a & b) | (b & cin) | (a & cin);
 
+
+// cin a b      sum cout
+// 0   0 0      0    0
+// 0   0 1      1    0
+// 0   1 0      1    0
+// 0   1 1      0    1
+// 1   0 0      1    0
+// 1   0 1      0    1
+// 1   1 0      0    1
+// 1   1 1      1    1
 endmodule
